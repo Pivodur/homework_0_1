a788490c0a292d646ff6e433284fcf28718a706a
Pivodur <megazyamba@gmail.com>
2023-01-20 15:06:33 +0600

    feat: add new responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 $(function(){
-    var mixer = mixitup('.gallery__content');
     $('.slider-blog__inner').slick({
         dots: true,
         arrows: false,
     });
+
+    $('.menu__btn').on('click',function(){
+        $('.header__top-inner').toggleClass('header__top-inner--active');
+    });
+
+    var mixer = mixitup('.gallery__content');
 });
 
